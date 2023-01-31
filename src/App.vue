418dab84d479ddf5774b6509ea7e655755c76cb6
Luciano Ferraz Mendes <luciano.mendes@compasso.com.br>
2023-01-31 10:17:41 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+           <TodoSpinner v-if="loading"/>
+            <template  v-else>
+                <TodoFormAdd/>
+                <TodoItems v-if="$store.state.todos.length" />
+                <TodoEmpty v-else/>
+            </template>          
+
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoEmpty from './components/TodoEmpty.vue';
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
+    data() {
+        return {
+            loading: false,
+    }},
+    created() 
+    {
+        this.loading = true;
+        this.$store.dispatch('getTodos').finally(() =>{
+            this.loading = false;
+        })
+            
+    },
 }
 </script>
 
