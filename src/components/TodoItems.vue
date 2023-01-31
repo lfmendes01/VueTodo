8c455ae0a255a8b8760108fecd28b4978a9b3fbc
Luciano Ferraz Mendes <luciano.mendes@compasso.com.br>
2023-01-31 10:59:55 -0300

    first project vue

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,17 @@
+
+import TodoItem from './TodoItem.vue';
+
+<template>
+    <TodoItem v-for="todo in $store.state.todos"
+    :key="todo.id" :todo="todo"/>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue';
+
+
+export default {
+    name: "App",
+    components: { TodoItem }
+}
+</script>
